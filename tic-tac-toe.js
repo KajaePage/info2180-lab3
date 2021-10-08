750e394d87a3fdf47bbcdd63baa8264421d3acaf
Kajae <kajaepage21@gmail.com>
2021-10-07 22:25:51 -0500

    Added implementation of an X or O to a square when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,13 +1,35 @@
+let currentTurn;
 document.addEventListener("DOMContentLoaded", loadDOM)
 
+
 function loadDOM(){ 
-    showBoard() 
+
+    showBoard()
+    plays()
+}
+
+function showBoard(){ 
+
+    const boarddivs = document.getElementById('board').getElementsByTagName('div');
+
+    for( i=0; i< boarddivs.length; i++ ){
+        var divs = boarddivs[i];
+        divs.classList.add("square");
+        console.log("Board has Loaded!")
+    }
+}
+
+function plays(){
+
+    const boarddivs = document.getElementById('board').getElementsByTagName('div');
+    for(const div of boarddivs){
+        div.classList.remove("X","O");
+        div.addEventListener("click", (e) =>{
+            div.classList.add(currentTurn);
+            div.textContent = currentTurn;
+            currentTurn = currentTurn == "X" ? "O" : "X";
+        });
     }
 
-    function showBoard(){ 
-        var boarddiv= document.getElementById("board");
-        for ( i =0; i< 9; i++){
-	        var divs = boarddiv.getElementsByTagName("div")[i];
-        	divs.classList.add("square");
-        }
-     }
+    currentTurn = Math.round(Math.random(0,1)) == 1 ? "X" : "O";
+}
